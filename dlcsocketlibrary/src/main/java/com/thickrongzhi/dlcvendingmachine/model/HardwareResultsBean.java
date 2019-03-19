7b99f27e6ad8ba28c48b9efd7d4a522284d78795
郭涛 <562559842@qq.com>
2019-03-19 09:42:41 +0800

    1

--- dlcsocketlibrary/src/main/java/com/thickrongzhi/dlcvendingmachine/model/HardwareResultsBean.java
+++ dlcsocketlibrary/src/main/java/com/thickrongzhi/dlcvendingmachine/model/HardwareResultsBean.java
@@ -6,7 +6,7 @@ public class HardwareResultsBean
     public int mCommands;
     public SignalOutput mSignalOutput;
     public SignalState mSignalState;
-    
+    @Override
     public String toString()
     {
         StringBuilder localStringBuilder = new StringBuilder();
